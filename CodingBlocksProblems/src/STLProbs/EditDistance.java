43d258e166e73d8deed2f12b54a2d816e8809ab1
abhishek <abugautam@gmail.com>
2018-12-20 22:39:24 +0530

    Edit distance bottom up dp added

--- CodingBlocksProblems/src/STLProbs/EditDistance.java
+++ CodingBlocksProblems/src/STLProbs/EditDistance.java
@@ -16,7 +16,8 @@ public class EditDistance {
 		int n=str2.length();
 		
 		int edits = MinEdits(str1,str2,m,n);
-		System.out.print(edits);
+		System.out.println("Min edits top down dp : "+edits);
+		System.out.println("Min edits bottom up dp : "+MinEditsBottomUpDp(str1, str2));
 	}
 	
 	static int MinEdits(String str1,String str2,int m,int n)
@@ -53,6 +54,37 @@ public class EditDistance {
 		return dp[m][n];
 	}
 	
-	
+	static int MinEditsBottomUpDp(String str1,String str2)
+	{
+		int m = str1.length();
+		int n = str2.length();
+		
+		int[][] dp= new int[m+1][n+1];
+		
+		for(int i=0;i<=m;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				// When either of str length is 0
+				if(i==0)
+				{
+					dp[i][j]=j;
+				}
+				else if(j==0)
+				{
+					dp[i][j]=i;
+				}
+				else if(str1.charAt(i-1)==str2.charAt(j-1))
+				{
+					dp[i][j] = dp[i-1][j-1];
+				}
+				else {
+					dp[i][j] = 1+Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+				}
+			}
+		}
+		
+		return dp[m][n];
+	}
 
 }
